95926a4ee4202f8e33af84183993a4981aacfdc4
Stanislav Bardyuk <6258615+kodlan@users.noreply.github.com>
2023-04-02 20:58:11 +0300

    Box spirals demo (#2)
    * Initial project setup
    
    * Format java files
    
    * Java version updated
    
    * Box demo
    
    * Bug fix
    
    * Spin in different directions
    
    * Refactorings

--- core/src/com/graphdemos/DemoMain.java
+++ core/src/com/graphdemos/DemoMain.java
@@ -1,32 +1,153 @@
 package com.graphdemos;
 
+import static com.graphdemos.util.ColorUtil.HSBtoRGB;
+
 import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer.ShapeType;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.stream.Collectors;
 
 public class DemoMain extends ApplicationAdapter {
 
-  SpriteBatch batch;
-  Texture img;
+  private static final int ANGLE_STEP = 5;
+  private static final int BOX_SIZE_STEP = 5;
+  private static final int BOX_START_SIZE = 30;
+
+  private ShapeRenderer shapeRenderer;
+
+  private int width;
+  private int height;
+
+  private final Random rnd = new Random();
+
+  private final Color currentColor = new Color();
+
+  List<Spiral> spiralList = new ArrayList<>();
 
   @Override
   public void create() {
-    batch = new SpriteBatch();
-    img = new Texture("badlogic.jpg");
+    shapeRenderer = new ShapeRenderer();
+
+    width = Gdx.graphics.getWidth();
+    height = Gdx.graphics.getHeight();
+
+    int centerX = width / 2;
+    int centerY = height / 2;
+
+    System.out.println("width = " + Gdx.graphics.getWidth());
+    System.out.println("height = " + Gdx.graphics.getHeight());
+    System.out.println("centerX = " + centerX + ", centerY = " + centerY);
+
+    addSpiral();
+    addSpiral();
+    addSpiral();
+
+    Gdx.gl.glClearColor(0f, 0f, 0f, 1.0f);
+    Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
   }
 
   @Override
   public void render() {
-    ScreenUtils.clear(1, 0, 0, 1);
-    batch.begin();
-    batch.draw(img, 0, 0);
-    batch.end();
+    super.render();
+
+    shapeRenderer.begin(ShapeType.Line);
+    renderSpirals();
+    shapeRenderer.end();
+  }
+
+  private void addSpiral() {
+    spiralList.add(new Spiral(rnd.nextInt(width), rnd.nextInt(height), 5 + rnd.nextInt(BOX_START_SIZE),
+        rnd.nextInt() % 2 == 0));
+  }
+
+  private void addSpiralIfNone() {
+    if (spiralList.size() == 0) {
+      addSpiral();
+    }
+  }
+
+  private void renderSpirals() {
+    spiralList.forEach(this::nextSpiralFrame);
+    removeFinishedSpirals();
+    addSpiralIfNone();
+  }
+
+  private void removeFinishedSpirals() {
+    spiralList = spiralList.stream()
+        .filter(this::isSpiralNotFinished)
+        .collect(Collectors.toList());
+  }
+
+  private void nextSpiralFrame(Spiral spiral) {
+    drawSpiralFrame(spiral);
+    cycleSpiral(spiral);
+  }
+
+  /**
+   * Draw only one rotation of the given spiral
+   */
+  private void drawSpiralFrame(Spiral spiral) {
+    Color.argb8888ToColor(currentColor, HSBtoRGB(spiral.currentAngle / 360f, 1, 1));
+    shapeRenderer.setColor(currentColor);
+    shapeRenderer.rect(spiral.centerX - spiral.currentBoxSize, spiral.centerY - spiral.currentBoxSize,
+        spiral.currentBoxSize, spiral.currentBoxSize, spiral.currentBoxSize * 2, spiral.currentBoxSize * 2,
+        1f, 1f, spiral.currentAngle);
+  }
+
+  private void cycleSpiral(Spiral spiral) {
+    if (spiral.forwardDirection) {
+      spiral.currentAngle += ANGLE_STEP;
+    } else {
+      spiral.currentAngle -= ANGLE_STEP;
+    }
+    spiral.currentBoxSize += BOX_SIZE_STEP;
+  }
+
+  private boolean isSpiralNotFinished(Spiral spiral) {
+    return spiral.currentBoxSize < 360;
+  }
+
+  /**
+   * Draw full spiral
+   */
+  private void drawSpiral(int centerX, int centerY, int boxStartSize) {
+    float size = boxStartSize;
+    Color color = new Color();
+
+    for (int angle = 0; angle < 360; angle += ANGLE_STEP, size += BOX_SIZE_STEP) {
+      Color.argb8888ToColor(color, HSBtoRGB(angle / 360f, 1, 1));
+      shapeRenderer.setColor(color);
+      shapeRenderer.rect(centerX - size, centerY - size, size, size, size * 2, size * 2, 1f, 1f, angle);
+    }
   }
 
   @Override
   public void dispose() {
-    batch.dispose();
-    img.dispose();
+    super.dispose();
+    shapeRenderer.dispose();
+  }
+
+  private static class Spiral {
+
+    int centerX;
+    int centerY;
+    int currentBoxSize;
+    int currentAngle;
+
+    boolean forwardDirection;
+
+    public Spiral(int centerX, int centerY, int initBoxSize, boolean forwardDirection) {
+      this.centerX = centerX;
+      this.centerY = centerY;
+      this.currentBoxSize = initBoxSize;
+      this.currentAngle = 0;
+      this.forwardDirection = forwardDirection;
+    }
   }
 }
--- desktop/src/com/graphdemos/DesktopLauncher.java
+++ desktop/src/com/graphdemos/DesktopLauncher.java
@@ -11,6 +11,7 @@ public class DesktopLauncher {
     Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
     config.setForegroundFPS(60);
     config.setTitle("GraphDemos");
+    config.setWindowedMode(800, 600);
     new Lwjgl3Application(new DemoMain(), config);
   }
 }
